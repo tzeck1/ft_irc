062f42dbf95ecb6779ccfecec375a9845ef92858
tom <icekex23@gmail.com>
2022-11-02 12:40:42 +0100

    progress

--- include/common.hpp
+++ include/common.hpp
@@ -6,16 +6,26 @@
 /*   By: tzeck <@student.42heilbronn.de>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 10:19:35 by tzeck             #+#    #+#             */
-/*   Updated: 2022/11/02 10:30:12 by tzeck            ###   ########.fr       */
+/*   Updated: 2022/11/02 12:34:57 by tzeck            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 /*------	INCLUDES	-----*/
-#include <sys/socket.h>
+#include <sys/socket.h> // socket functions
+#include <netinet/in.h> // sockaddr struct
+ #include <sys/types.h>
+
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+
+// #include <netdb.h>
 #include <iostream>
-#include <string>
+#include <string.h>
+#include <unistd.h>
 
 /*------	MACROS	-----*/
-#define SERVER_PORT 80
+#define SERVER_PORT 420
+#define MAXLINE		4096
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: tzeck <@student.42heilbronn.de>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 10:29:09 by tzeck             #+#    #+#             */
-/*   Updated: 2022/11/02 10:39:32 by tzeck            ###   ########.fr       */
+/*   Updated: 2022/11/02 12:40:25 by tzeck            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,40 @@
 
 int main(int argc, char **argv)
 {
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	int sockfd, connfd, n;
+	uint8_t	buff[MAXLINE + 1];
+	uint8_t	recvline[MAXLINE + 1];
+	
+	// AF_INET = internet connection; SOCK_STREAM = R/W Stream; 0 = TCP
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	struct sockaddr_in servaddr;
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET; // IP-Category -> IPV4
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); // listn to all clients
+	servaddr.sin_port = htons(SERVER_PORT); // our port :)
+
+	bind(sockfd, (sockaddr *)&servaddr, sizeof(servaddr)); // assign address to our socket
+	listen(sockfd, 10); // socket can recieve connections
+
+	std::cout << "server running ..." << std::endl;
+	while(true)
+	{
+		struct sockaddr_in	addr;
+		socklen_t			addr_len;
+
+		connfd = accept(sockfd, (sockaddr *)NULL, NULL);
+		memset(recvline, 0, MAXLINE);
+		
+		while ((n = read(connfd, recvline, MAXLINE - 1)) > 0)
+		{
+			std::cout << recvline << std::endl;
+			if (recvline[n - 1] == '\n')
+				break ;
+			memset(recvline, 0, MAXLINE);
+		}
+		write(connfd, "HELLO!", 6);
+		close(connfd);
+	}
 	return (EXIT_SUCCESS);
 }
