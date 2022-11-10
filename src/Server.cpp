1db740ffc52c0233c1828e31096e94442f2197e7
Ben Tenzlinger <btenzlin@1-D-29.42heilbronn.de>
2022-11-10 10:17:21 +0100

    changed do whiles to whiles

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: btenzlin <btenzlin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 12:05:31 by btenzlin          #+#    #+#             */
-/*   Updated: 2022/11/08 19:12:46 by btenzlin         ###   ########.fr       */
+/*   Updated: 2022/11/10 10:16:49 by btenzlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,8 +67,8 @@ void	Server::init_socket(void)
 	_socket_addr.sin_family = AF_INET;
 	_socket_addr.sin_addr.s_addr = htonl(INADDR_ANY); // listn to all clients
 	_socket_addr.sin_port = htons(_server_port); // our port :)
-	std::cout << "Port: " << _server_port << std::endl;
-	std::cout << "End of init socket" << std::endl;
+	// std::cout << "Port: " << _server_port << std::endl;
+	// std::cout << "End of init socket" << std::endl;
 }
 
 void	Server::bind_and_listen(void)
@@ -93,7 +93,7 @@ void	Server::bind_and_listen(void)
 
 	_client_fds[0].fd = _socket_fd;
 	_client_fds[0].events = POLLIN;
-	std::cout << "End of bind and listen" << std::endl;
+	// std::cout << "End of bind and listen" << std::endl;
 }
 
 int		Server::check_connection(int i)
@@ -106,16 +106,16 @@ int		Server::check_connection(int i)
 		_end_server = true;
 		return (0);
 	}
-	std::cout << "End of check connection" << std::endl;
+	// std::cout << "End of check connection" << std::endl;
 	return (2);
 }
 
 void		Server::accept_connection(void)
 {
-	int	new_fd;
+	int	new_fd = -2;
 
 	std::cerr << "Listening socket is readable" << std::endl;
-	do
+	while (new_fd != -1)
 	{
 		new_fd = accept(_socket_fd, NULL, NULL);
 		if (new_fd < 0)
@@ -133,8 +133,8 @@ void		Server::accept_connection(void)
 		_client_fds[_client_count].events = POLLIN;
 		_client_count++;
 
-	} while (new_fd != -1);
-	std::cout << "End of accept" << std::endl;
+	}
+	// std::cout << "End of accept" << std::endl;
 }
 
 void	Server::receive_msg(int i)
@@ -148,7 +148,7 @@ void	Server::receive_msg(int i)
 	std::cout << "Descriptor " << _client_fds[i].fd << " is readable" << std::endl;
 	_close_conn = false;
 
-	do
+	while (true)
 	{
 		sleep(1);
 		err = recv(_client_fds[i].fd, buffer, sizeof(buffer), 0);
@@ -170,9 +170,17 @@ void	Server::receive_msg(int i)
 		else if (err < 80)
 			buffer[err] = '\0';
 		_recv_msg += buffer;
-		std::cout << "Recv_msg: " << _recv_msg;
-		std::cout << err << " bytes received" << std::endl;
-	} while (true);
+		// std::cout << "Recv_msg: " << _recv_msg;
+		// std::cout << err << " bytes received" << std::endl;
+	}
+
+	// std::cout << "RECEIVE: " << _recv_msg << std::endl;
+	/* *TESTING* */
+	// User user = welcome_msg(_recv_msg, _socket_addr.sin_addr.s_addr); //throws exception!
+	// std::string welcome = user.build_reply();
+	// send(_client_fds[i].fd, welcome.c_str(), 94, 0);
+	/* *TESTING* */
+
 	pos = _recv_msg.find("\r\n");
 	while (pos >= 0)
 	{
@@ -189,7 +197,7 @@ void	Server::receive_msg(int i)
 		_client_fds[i].fd = -1;
 		_compress_array = true;
 	}
-	std::cout << "End of receive" << std::endl;
+	// std::cout << "End of receive" << std::endl;
 }
 
 void	Server::handle_connections(void)
@@ -197,10 +205,10 @@ void	Server::handle_connections(void)
 	int	err;
 	int	current_size;
 
-	do {
+	while (!_end_server) {
 		std::cout << "Waiting on poll..." << std::endl;
 		err = poll(&_client_fds[0], _client_count, _timeout);
-		std::cout << "Poll was called" << std::endl;
+		// std::cout << "Poll was called" << std::endl;
 		if (err < 0)
 		{
 			std::cerr << "Poll failed" << std::endl;
@@ -238,7 +246,7 @@ void	Server::handle_connections(void)
 				}
 			}
 		}
-	} while (!_end_server);
+	}
 	for (int i = 0; i < _client_count; i++)
 	{
 		if (_client_fds[i].fd >= 0)
